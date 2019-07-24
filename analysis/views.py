21a602d29d21f48c1aa833c4db0c4802be681389
server <2@2.com>
2019-07-24 15:55:04 +0800

    redis 计数

--- analysis/views.py
+++ analysis/views.py
@@ -7,9 +7,8 @@ from redis.sentinel import Sentinel
 sentinel = Sentinel([('sentinel', 26379)], socket_timeout=0.1)
 rds = sentinel.master_for('master', socket_timeout=0.1)
 
-def ping(request): 
-    # rds.incr('count', 1)
-    # cnt = rds.get('count')
-    # cnt = '0' if cnt is None else cnt
-    # return HttpResponse(cnt)
-    return sentinel.discover_master('master')
+def ping(request):
+    rds.incr('count', 1)
+    cnt = rds.get('count')
+    cnt = '0' if cnt is None else cnt
+    return HttpResponse(cnt)
