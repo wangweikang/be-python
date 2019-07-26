f64607e408752c75545a179d6acc6f9f6f58e8f8
server <2@2.com>
2019-07-26 17:52:43 +0800

    url

--- checkin/urls.py
+++ checkin/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 
-from . import views
+from checkin.views import CheckInCreate, CommentCreate, AuthorUpdate
 
 urlpatterns = [
     # path('', views.index, name='index'),
