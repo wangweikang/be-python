596b1444d85fac6793a84023561b5df567ea2153
server <2@2.com>
2019-07-26 18:04:18 +0800

    test

--- checkin/urls.py
+++ checkin/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
 
-from checkin.views import CheckInCreate, CommentCreate, AuthorUpdate
+from checkin.views import CheckInCreate, CommentCreate, CommentUpdate
 
 urlpatterns = [
     # path('', views.index, name='index'),
     path('/', CheckInCreate.as_view(), name='checkin'),
     path('/comments', CommentCreate.as_view(), name='comments'),
-    path('/comments/<int:pk>/', AuthorUpdate.as_view(), name='update'),
+    path('/comments/<int:pk>/', CommentUpdate.as_view(), name='update'),
 ]
