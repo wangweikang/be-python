dbae9161612d9ab851ae7d1eda588a4de18a7287
server <2@2.com>
2019-07-26 18:25:28 +0800

    test

--- checkin/urls.py
+++ checkin/urls.py
@@ -4,7 +4,7 @@ from checkin.views import CheckInCreate, CommentCreate, CommentUpdate
 
 urlpatterns = [
     # path('', views.index, name='index'),
-    path('/', CheckInCreate.as_view(), name='checkin'),
-    path('/comments', CommentCreate.as_view(), name='comments'),
-    path('/comments/<int:pk>/', CommentUpdate.as_view(), name='update'),
+    path('', CheckInCreate.as_view(), name='checkin'),
+    path('comments', CommentCreate.as_view(), name='comments'),
+    path('comments/<int:pk>/', CommentUpdate.as_view(), name='update'),
 ]
